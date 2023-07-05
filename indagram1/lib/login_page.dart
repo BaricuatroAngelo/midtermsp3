70de36060ade19b45f22294913f6fb51f135f409
Angelo <angelolouis.baricuatro.20@usjr.edu.ph>
2023-07-05 12:13:01 +0800

    connected to firebase, no display error

---
+++ indagram1/lib/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+class LoginPage extends StatelessWidget{
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
